c12f7b222daf02a295ac67e6a215f1253ced4ab4
Vai na Web <vainawebcpx@gmail.com>
2018-03-06 17:04:10 -0300

    quase pronto

--- src/components/Calc.vue
+++ src/components/Calc.vue
@@ -1,74 +1,99 @@
 <template>
-	<div class="base">
-		<header class="titulo">
+<div id="appCalc">
+	<main class="principal">
+		<!-- <header class="titulo">
 			<h1>Calculadora</h1>
 		</header>
-		<main class="container">
+		<main class="conteiner"> -->
 		<section class="inputs">
-			<input class="input" type="number" name="calc1" v-model="valor1">
-			<input class="input" type="number" name="calc2" v-model="valor2">
-			<input class="input" type="number" name="calc3" v-model="resultados">
+			<p class="input">{{ result }}</p>
+			<input class="input" type="number" v-model.number="valor1">
+			<input class="input" type="number" v-model.number="valor2">
 		</section>
-		<section class="conteiner-do-button">
-			<button class="botao botaoZerar">C</button>
-			<button class="botao">+</button>
-			<button class="botao">-</button>
-			<button class="botao">x</button>
-			<button class="botao">/</button>
+		<section class="botoes">
+			<button class="botao" @click="calc('soma')">+</button>
+			<button class="botao" @click="calc('subtaçao')">-</button>
+			<button class="botao" @click="calc('multiplicaçao')">×</button>
+			<button class="botao" @click="calc('divisao')">÷</button>
 		</section>
-		</main>
-	</div>
+		<!-- </main> -->
+	</main>
+</div>
 </template>
 <script>
 	export default{
 		name:'Calc',
 		data(){
-		 return{
-		 	valor1:0,
-		 	valor2:0,
-		 	resultado:null
+			return{
+			 	valor1:'',
+			 	valor2:'',
+			 	result:''
+			}
+		},
+		methods:{
+			calc(operator){
+				if (operator === 'soma') {
+					this.result = this.valor1 + this.valor2
+				}else if(operator === 'subtaçao'){
+					this.result = this.valor1 - this.valor2
+				}else if (operator === 'multiplicaçao') {
+					this.result = this.valor1 * this.valor2 
+				}else{
+					this.result = this.valor1 / this.valor2
+				}
+
 			}
 		}
 	}
 </script>
 <style scoped>
-	.base {
-		background-color: black;
+	*{
+		margin:0;
+		padding: 0;
 	}
-	.titulo{
-		color: blue;
-	}
-	.container{
+	#appCalc{
+		width: 100%;
 		display: flex;
 		justify-content: center;
 		align-items: center;
-		flex-direction: column;	
+	}
+	.principal{
+		max-width: 300px;
+		background: #2A3137;
+		color: #6FB0E2;
+	}
+
+	.titulo{
+		color: #6FB0E2;
+	}
+
+	.conteiner{
+		/*padding: 30px;*/
 	}
 	.inputs{
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		flex-direction: column;
-		width: 60%;
+		
 	}
 	.input{
-		margin: 10px;
-		width: 100%;
-		height: 5vh;
-
-	}
-	.conteiner-do-button{
+		background: #9EA2A6;
+		color: #2A3137;
+		padding:20px;
+		margin: 5px;
+		border: none;
 		width: 60%;
+	}
+	.botoes{
 		display: flex;
-		justify-content: center;
-		align-items: center;
 		flex-wrap: wrap;
+		margin: 0;
+		padding: 0;
 	}
 	.botao{
-		width: 50%;
-		height: 10vh;
-	}
-	.botaoZerar{
-		width: 100%;
+		background: #2A3137;
+		color: #6FB0E2;
+		padding:20px;
+		/*margin-left: 11px;*/
+		border: none;
+		width: 150px;
+		height: 150px;		
 	}
 </style>
