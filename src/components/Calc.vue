110bedb0e7842a740b7af4ef362c10bdc46db52e
Vai na Web <vainawebcpx@gmail.com>
2018-03-07 15:34:39 -0300

    primeira calculadora pronta

--- src/components/Calc.vue
+++ src/components/Calc.vue
@@ -1,14 +1,10 @@
 <template>
 <div id="appCalc">
 	<main class="principal">
-		<!-- <header class="titulo">
-			<h1>Calculadora</h1>
-		</header>
-		<main class="conteiner"> -->
 		<section class="inputs">
-			<p class="input">{{ result }}</p>
-			<input class="input" type="number" v-model.number="valor1">
-			<input class="input" type="number" v-model.number="valor2">
+			<p class="inputsItem resultado">{{ result }}</p>
+			<input class="inputsItem input" type="number" v-model.number="valor1">
+			<input class="inputsItem input" type="number" v-model.number="valor2">
 		</section>
 		<section class="botoes">
 			<button class="botao" @click="calc('soma')">+</button>
@@ -16,7 +12,6 @@
 			<button class="botao" @click="calc('multiplicaçao')">×</button>
 			<button class="botao" @click="calc('divisao')">÷</button>
 		</section>
-		<!-- </main> -->
 	</main>
 </div>
 </template>
@@ -25,9 +20,9 @@
 		name:'Calc',
 		data(){
 			return{
-			 	valor1:'',
-			 	valor2:'',
-			 	result:''
+			 	valor1:0,
+			 	valor2:0,
+			 	result:0
 			}
 		},
 		methods:{
@@ -62,24 +57,25 @@
 		background: #2A3137;
 		color: #6FB0E2;
 	}
-
-	.titulo{
-		color: #6FB0E2;
-	}
-
-	.conteiner{
-		/*padding: 30px;*/
-	}
 	.inputs{
-		
+		background: #363F47;
+		padding: 10px;
+	}
+	.inputsItem{
+		background: #363F47;
+		color: #6FB0E2;
+		border: none;
+		width: 90%;
+		padding: 5%;
+	}
+	.resultado{
+		color: white;
+		font-size: 10vh;
 	}
 	.input{
-		background: #9EA2A6;
-		color: #2A3137;
-		padding:20px;
-		margin: 5px;
-		border: none;
-		width: 60%;
+		display: flex;
+		text-align: right;
+		font-size: 3vh;
 	}
 	.botoes{
 		display: flex;
@@ -91,7 +87,6 @@
 		background: #2A3137;
 		color: #6FB0E2;
 		padding:20px;
-		/*margin-left: 11px;*/
 		border: none;
 		width: 150px;
 		height: 150px;		
